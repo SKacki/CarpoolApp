e8c71de1c7f43bd76b1bb4d1ac3800377fee32d3
Szymon Kącki <10537@student.puzim.edu.pl>
2023-04-07 16:52:13 +0200

    Firebase Database configuration

--- app/src/main/java/com/inzynierka/carpoolapp/TestActivity.kt
+++ app/src/main/java/com/inzynierka/carpoolapp/TestActivity.kt
@@ -2,10 +2,24 @@ package com.inzynierka.carpoolapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.inzynierka.carpoolapp.databinding.ActivityTestBinding
+
+private lateinit var binding: ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //test database
+        binding.testshit.setOnClickListener {
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+            myRef.setValue("Hello, World!")
+
+        }
     }
 }
